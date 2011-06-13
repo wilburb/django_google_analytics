1d1f133643670cafd87603804337c30a7dabca45
Benjamin Wilbur <wilburb@wilburweb.com>
2011-06-13 00:13:31 -0500

    Added inline admin code to add google analytics web property id to the Site admin page.

--- admin.py
+++ admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from django_site_googleanalytics.models import GoogleAnalytics
+from django.contrib.sites import admin
 
-admin.site.register(GoogleAnalytics)
+class DjangoSiteGoogleAnalyticsInline(admin.StackedInline):
+    model = GoogleAnalytics
+    
+class SiteAdmin(admin.ModelAdmin):
+    inlines = [DjangoSiteGoogleAnalyticsInline]
+    
+admin.site.unregister(Site)
+admin.site.register(Site, SiteAdmin)
