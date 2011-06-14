2005c77be19e4900412b04fec62d329c7f8c4aed
Benjamin Wilbur <wilburb@wilburweb.com>
2011-06-13 22:59:11 -0500

    models.py modified to make a site the foreign key to each google analytics web property id admin.py inline set up to include Google analytics inline with the django admin site

--- admin.py
+++ admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django_site_googleanalytics.models import GoogleAnalytics
-from django.contrib.sites import admin
+from django.contrib.sites.admin import Site
 
-class DjangoSiteGoogleAnalyticsInline(admin.StackedInline):
+class DjangoSiteGoogleAnalyticsInline(admin.TabularInline):
     model = GoogleAnalytics
-    
+
 class SiteAdmin(admin.ModelAdmin):
     inlines = [DjangoSiteGoogleAnalyticsInline]
-    
+
 admin.site.unregister(Site)
 admin.site.register(Site, SiteAdmin)
